21d582234f3072449cc5cb149ce447851e44408e
정시현 <54366260+jungsiroo@users.noreply.github.com>
2022-07-24 23:27:56 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,102 +1,52 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-// const DB = require("../common/database");
+const Util = require("../common/util");
+const UserRepo = require("../repository/user");
 
-//https://day.js.org/docs/en/parse/string-format
-const dayjs = require('dayjs')
-
-/* GET users listing. */
-// router.get('/', function (req, res, next) {
-//     res.send('respond with a resource');
-// });
-
-router.get("/", (req, res) => {
-    const users = [{
-        id: 1,
-        name: "홍길동",
-        email: "abc@gmail.com",
-        studentId: "12345",
-        type: "관리자",
-        generation: "21"
-    }, {
-        id: 2,
-        name: "김이름",
-        email: "xyz@gmail.com",
-        studentId: "11111",
-        type: "일반유저",
-        generation: "21",
-        company: "BBBBBBBBB"
-    }, {
-        id: 3,
-        name: "가나다",
-        email: "hhh@gmail.com",
-        studentId: "121111",
-        type: "일반유저",
-        generation: "21",
-        company: "AAA"
+router.get('/', async (req, res) => {
+    try {
+        let ret = await UserRepo.getAllUsers({forAdmin:false});
+        res.status(ret.status).json(ret);
     }
-    ];
+    
+    catch (error) {
+        res.status(500).json(
+            Util.getReturnObject(error, 500, {})
+        )
+    }
+});
 
-    res.json({
-        users
-    });
-})
-
-
-//유저 상세 조회
 router.get("/:userId", async (req, res) => {
     const userId = req.params.userId;
 
     try {
-        //DB.execute의 결과가 [{user_id:123}]; 이런식이라서
-        //구조분해할당으로 바로 꺼낸다.
-        const [user] = await DB.execute({
-            psmt: `select * from USER where user_id = ?`,
-            binding: [userId]
-        });
-        //console.log("user: ", JSON.stringify(user)와 동일
-        console.log("user: %j", user);
-
-        if (!user) {
-            return res.status(404).json({
-                ok: false,
-                message: "해당 유저를 찾을 수 없습니다.",
-            });
-        }
-
-        res.json({
-            ok: true,
-
-            id: user.user_id,
-            name: user.name,
-            student_id: user.student_id,
-
-            //즉시실행함수
-            type: ((type) => {
-                if (!type) {
-                    return "일반유저";
-                }
-
-                switch (type) {
-                    case "sub":
-                        return "부관리자";
-                    case "primary":
-                        return "부관리자";
-                    default:
-                        return "unknown";
-                }
-            })(user.type),
-
-            applyDate: dayjs(user.createdAt).format("YY-MM-DD"),
-        })
+        let user = await UserRepo.getUser({userId:userId, forAdmin:false});
+        res.status(user.status).json(user);
+        
     } catch (e) {
-        console.error(e);
+        console.log(e);
+        res.status(500).json(
+            Util.getReturnObject("알 수 없는 오류가 발생했습니다.", 500, {})
+        )
+    }
+});
 
-        res.status(500).json({
-            ok: false,
-            message: "알 수 없는 오류가 발생했습니다."
-        });
+router.patch('/:userId', async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        let result = await UserRepo.editUser({
+            userId : userId,
+            data : req.body
+        })
+
+        res.status(result.status).json(result);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json(
+            Util.getReturnObject("알 수 없는 오류가 발생했습니다.", 500, {})
+        )
     }
 });
 
