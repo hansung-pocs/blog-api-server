82842d4ca4ebd15b4abfee1cc17a652ce2b4898b
10000DOO <gunjoun99@gmail.com>
2022-07-25 18:32:59 +0900

    dummy

---
+++ common/message.js
@@ -0,0 +1,23 @@
+const CANT_READ_USERDATA = "유저 목록을 불러올 수 없습니다."
+const CANT_READ_POSTDATA = "게시글 목록을 불러올 수 없습니다."
+const READ_USERDATA_SUCCESS = "유저 목록 조회 성공"
+const READ_POSTDATA_SUCCESS = "게시글 목록 조회 성공"
+const NO_USER_DATA = "없거나 탈퇴한 유저입니다."
+const NO_POST_DATA = "없거나 삭제된 게시글입니다."
+const READ_USER_SUCCESS = "님 조회 성공"
+const READ_POST_SUCCESS = "글 조회 성공"
+const UNKNOWN_ERROR = "알 수 없는 오류가 발생했습니다."
+const USER_UPDATE_SUCCESS = "정보가 업데이트되었습니다."
+
+module.exports = {
+    CANT_READ_USERDATA,
+    CANT_READ_POSTDATA,
+    READ_USERDATA_SUCCESS,
+    NO_USER_DATA,
+    NO_POST_DATA,
+    READ_USER_SUCCESS,
+    READ_POST_SUCCESS,
+    UNKNOWN_ERROR,
+    USER_UPDATE_SUCCESS,
+    READ_POSTDATA_SUCCESS
+}
--- routes/users.js
+++ routes/users.js
@@ -54,17 +54,18 @@ router.get('/', async (req, res) => {
     try{
         if(!usersDB){
             res.status(404).json({
-                    message: "잘못된 요청입니다.",
-                    status: 404,
-                    servertime: new Date()
-                });
+                message: "유저 목록을 불러올 수 없습니다.",
+                status: 404,
+                servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+                data: {}
+            });
         }
 
         let users = new Array();
         for(let i in usersDB){
             let usersObj = new Object();
             usersObj.userId = usersDB[i].userId;
-            usersObj.username = usersDB[i].username;
+            usersObj.userName = usersDB[i].username;
             usersObj.email = usersDB[i].email;
             usersObj.studentId = usersDB[i].studentId;
             usersObj.type = ((type) => {
@@ -81,15 +82,15 @@ router.get('/', async (req, res) => {
             usersObj.company = usersDB[i].company;
             usersObj.generation = usersDB[i].generation;
             usersObj.github = usersDB[i].github;
-            usersObj.createdAt = dayjs(usersDB[i].createdAt).format("YY-MM-DD");
+            usersObj.createdAt = dayjs(usersDB[i].createdAt).format("YYYY-MM-DD");
 
             users.push(usersObj);
         }
 
         res.status(200).json({
-            message: "유저 목록 조회 완료",
+            message: "유저 목록 조회 성공",
             status: 200,
-            servertime: new Date(),
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
             data: {
                 users
             }
@@ -99,23 +100,27 @@ router.get('/', async (req, res) => {
         res.status(500).json({
             message: "알 수 없는 오류가 발생했습니다.",
             status: 500,
-            servertime: new Date()
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+            data: {}
         });
     }
 });
 
 // 유저 기수/힉번 정렬
-router.get('/sort', async (req, res) => {
-    let option = req.query.option;
+router.get('/', async (req, res) => {
+    let option = req.query.sort;
     try{
         if(option == "studentId"){
             usersDB.reverse();
         }
+        if(option == "generation"){
+        }
         if(!usersDB){
             res.status(404).json({
-                message: "잘못된 요청입니다.",
+                message: "유저 목록을 불러올 수 없습니다.",
                 status: 404,
-                servertime: new Date()
+                servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+                data: {}
             });
         }
 
@@ -123,7 +128,7 @@ router.get('/sort', async (req, res) => {
         for(let i in usersDB){
             let usersObj = new Object();
             usersObj.userId = usersDB[i].userId;
-            usersObj.username = usersDB[i].username;
+            usersObj.userName = usersDB[i].username;
             usersObj.email = usersDB[i].email;
             usersObj.studentId = usersDB[i].studentId;
             usersObj.type = ((type) => {
@@ -137,19 +142,19 @@ router.get('/sort', async (req, res) => {
                         return "unknown";
                 }
             })(usersDB[i].type);
-                usersObj.company = usersDB[i].company;
+            usersObj.company = usersDB[i].company;
             usersObj.generation = usersDB[i].generation;
             usersObj.github = usersDB[i].github;
-            usersObj.createdAt = dayjs(usersDB[i].createdAt).format("YY-MM-DD");
+            usersObj.createdAt = dayjs(usersDB[i].createdAt).format("YYYY-MM-DD");
 
             users.push(usersObj);
             usersDB.reverse();
         }
 
         res.status(200).json({
-            message: "유저 목록 정렬 완료",
+            message: "유저 목록 조회 성공",
             status: 200,
-            servertime: new Date(),
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
             data: {
                 users
             }
@@ -160,7 +165,8 @@ router.get('/sort', async (req, res) => {
         res.status(500).json({
             message: "알 수 없는 오류가 발생했습니다.",
             status: 500,
-            servertime: new Date()
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+            data: {}
         });
     }
 });
@@ -173,17 +179,18 @@ router.get("/:userId", async (req, res) => {
             res.status(404).json({
                 message: "없거나 탈퇴한 유저입니다.",
                 status: 404,
-                servertime: new Date()
+                servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+                data: {}
             });
         }
 
         res.status(200).json({
-            message: "유저 상세 조회",
+            message: `${usersDB[i].username}님 조회 성공`,
             status: 200,
-            servertime: new Date(),
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
             data: {
                 userId : usersDB[i].userId,
-                username : usersDB[i].username,
+                userName : usersDB[i].username,
                 email : usersDB[i].email,
                 studentId : usersDB[i].studentId,
                 type : ((type) => {
@@ -200,7 +207,7 @@ router.get("/:userId", async (req, res) => {
                 company : usersDB[i].company,
                 generation : usersDB[i].generation,
                 github : usersDB[i].github,
-                createdAt : dayjs(usersDB[i].createdAt).format("YY-MM-DD")
+                createdAt : dayjs(usersDB[i].createdAt).format("YYYY-MM-DD")
             }
         });
 
@@ -209,90 +216,103 @@ router.get("/:userId", async (req, res) => {
         res.status(500).json({
             message: "알 수 없는 오류가 발생했습니다.",
             status: 500,
-            servertime: new Date()
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+            data: {}
         });
     }
 });
 
 //유저 이름 검색
-router.get("/search/:name", async (req, res) => {
-    const name = decodeURIComponent(req.params.name);
-    let i;
-    try {
-        if (!usersDB) {
-            res.status(404).json({
-                message: "해당 유저를 찾을 수 없습니다.",
-                status: 404,
-                servertime: new Date()
-            });
-        }
-
-        if(name == "김철수"){
-            i = 0;
-        }else if(name == "홍길동"){
-            i = 1;
-        }else if(name == "김영희"){
-            i = 2;
-        }
-
-
-        res.status(200).json({
-            message: "유저 이름으로 검색",
-            status: 200,
-            servertime: new Date(),
-            data: {
-                userId : usersDB[i].userId,
-                username : usersDB[i].username,
-                email : usersDB[i].email,
-                studentId : usersDB[i].studentId,
-                type : ((type) => {
-                    if (!type) {
-                        return "일반유저";
-                    }
-                    switch (type) {
-                        case "primary":
-                            return "관리자";
-                        default:
-                            return "unknown";
-                    }
-                })(usersDB[i].type),
-                company : usersDB[i].company,
-                generation : usersDB[i].generation,
-                github : usersDB[i].github,
-                createdAt : dayjs(usersDB[i].createdAt).format("YY-MM-DD")
-            }
-        });
-
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({
-            message: "알 수 없는 오류가 발생했습니다.",
-            status: 500,
-            servertime: new Date()
-        });
-    }
-});
+// router.get("/search/:name", async (req, res) => {
+//     const name = decodeURIComponent(req.params.name);
+//     let i;
+//     try {
+//         if (!usersDB) {
+//             res.status(404).json({
+//                 message: "해당 유저를 찾을 수 없습니다.",
+//                 status: 404,
+//                 servertime: new Date()
+//             });
+//         }
+//
+//         if(name == "김철수"){
+//             i = 0;
+//         }else if(name == "홍길동"){
+//             i = 1;
+//         }else if(name == "김영희"){
+//             i = 2;
+//         }
+//
+//
+//         res.status(200).json({
+//             message: "유저 이름으로 검색",
+//             status: 200,
+//             servertime: new Date(),
+//             data: {
+//                 userId : usersDB[i].userId,
+//                 username : usersDB[i].username,
+//                 email : usersDB[i].email,
+//                 studentId : usersDB[i].studentId,
+//                 type : ((type) => {
+//                     if (!type) {
+//                         return "일반유저";
+//                     }
+//                     switch (type) {
+//                         case "primary":
+//                             return "관리자";
+//                         default:
+//                             return "unknown";
+//                     }
+//                 })(usersDB[i].type),
+//                 company : usersDB[i].company,
+//                 generation : usersDB[i].generation,
+//                 github : usersDB[i].github,
+//                 createdAt : dayjs(usersDB[i].createdAt).format("YY-MM-DD")
+//             }
+//         });
+//
+//     } catch (e) {
+//         console.error(e);
+//         res.status(500).json({
+//             message: "알 수 없는 오류가 발생했습니다.",
+//             status: 500,
+//             servertime: new Date()
+//         });
+//     }
+// });
 
 //유저 프로필 수정
 router.patch('/:userId', async (req,res) => {
     const password = req.body.password;
-    const username = req.body.username;
+    const userName = req.body.username;
     const email = req.body.email;
     const company = req.body.company;
     const github = req.body.github;
     const i = req.params.id;
     try{
-        console.log("%j",usersDB[i-1].password);
+        if (usersDB[i - 1].password == password && usersDB[i - 1].userName == userName &&
+            usersDB[i - 1].email == email && usersDB[i - 1].company == company &&
+            usersDB[i - 1].github == github
+            ) {
+            res.status(404).json({
+                message: "바뀔 값이 없습니다.",
+                status: 404,
+                servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+                data: {}
+            });
+        }
+
         usersDB[i-1].password = password;
-        usersDB[i-1].username = username;
+        usersDB[i-1].userName = userName;
         usersDB[i-1].email = email;
         usersDB[i-1].company = company;
         usersDB[i-1].github = github;
 
-        res.status(201).json({
-            message: "유저 정보 수정 완료",
-            status: 201,
-            servertime: new Date()
+        res.status(302).json({
+            message: "정보가 업데이트되었습니다.",
+            status: 302,
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+            data: {}
         });
         console.log("%j",usersDB);
     } catch (e){
@@ -300,7 +320,8 @@ router.patch('/:userId', async (req,res) => {
         res.status(500).json({
             message: "알 수 없는 오류가 발생했습니다.",
             status: 500,
-            servertime: new Date()
+            servertime: dayjs().format('YYYY-MM-DD HH:MM:ss'),
+            data: {}
         });
     }
 })
