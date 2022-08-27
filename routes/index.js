7ad6b198c520eccbd4a65be5708ae866cf35d8fd
acceptor-gyu <gseonggyu968@gmail.com>
2022-08-27 21:29:30 +0900

    비회원 유저 권한 제한

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,82 @@ const DB = require('../common/database');
 const dayjs = require('dayjs');
 const MSG = require('../common/message');
 const Util = require('../common/util');
+const {isLoggedIn} = require('../common/middlewares');
+
 
 /* GET home page. */
-router.get('/', async (req, res) => {
+router.get('/', isLoggedIn, async (req, res) => {
 
-        try {
-            const categoryList = ['notice', 'knowhow', 'reference', 'memory', 'study', 'qna'];
+    const user = req.user;
 
-            const bestPostDB = await DB.execute({
-                psmt: `select * from POST where canceled_at is NULL order by views DESC limit 0, 3;`,
-                binding: []
+    try {
+        const categoryList = ['notice', 'knowhow', 'reference', 'memory', 'study', 'qna'];
+
+        if (user.type === null) {
+            var sql1 = `select * from POST where canceled_at is NULL and only_member is false order by views DESC limit 0, 3`;
+        } else if (user.type === 'admin' || user.type === 'member') {
+            var sql1 = `select * from POST where canceled_at is NULL order by views DESC limit 0, 3`;
+        }
+
+        const bestPostDB = await DB.execute({
+            psmt: sql1,
+            binding: []
+        })
+
+        const bestPosts = [];
+
+        bestPostDB.forEach(bestPostDB => {
+            const {
+                post_id,
+                name,
+                title,
+                content,
+                views,
+                only_member,
+                created_at,
+                updated_at,
+                category
+            } = bestPostDB;
+
+            const bestPostsObj = {
+                postId: post_id,
+                writerName: name,
+                title: title,
+                content: content,
+                views: views,
+                onlyMember: !!only_member,
+                createdAt: dayjs(created_at).format('YYYY-MM-DD'),
+                updatedAt: ((updated_at) => {
+                    if (!!updated_at) {
+                        return dayjs(updated_at).format('YYYY-MM-DD')
+                    }
+                    return null;
+                })(updated_at),
+                category: (category)
+            }
+            bestPosts.push(bestPostsObj);
+        })
+
+        const noticePosts = [];
+        const knowhowPosts = [];
+        const referencePosts = [];
+        const memoryPosts = [];
+        const studyPosts = [];
+        const qnaPosts = [];
+
+        if (user.type === null) {
+            var sql2 = `select * from POST where category = ? and canceled_at is NULL and only_member is false order by created_at DESC limit 0, 3;`;
+        } else if (user.type === 'admin' || user.type === 'member') {
+            var sql2 = `select * from POST where category = ? and canceled_at is NULL order by created_at DESC limit 0, 3;`;
+        }
+
+        for (const categoryForBinding of categoryList) {
+            const postDB = await DB.execute({
+                psmt: sql2,
+                binding: [categoryForBinding]
             })
 
-            const bestPosts = [];
-
-            bestPostDB.forEach(bestPostDB => {
+            postDB.forEach(postDB => {
                 const {
                     post_id,
                     name,
@@ -30,9 +91,9 @@ router.get('/', async (req, res) => {
                     created_at,
                     updated_at,
                     category
-                } = bestPostDB;
+                } = postDB;
 
-                const bestPostsObj = {
+                const postsObj = {
                     postId: post_id,
                     writerName: name,
                     title: title,
@@ -48,80 +109,34 @@ router.get('/', async (req, res) => {
                     })(updated_at),
                     category: (category)
                 }
-                bestPosts.push(bestPostsObj);
-            })
-
-            const noticePosts = [];
-            const knowhowPosts = [];
-            const referencePosts = [];
-            const memoryPosts = [];
-            const studyPosts = [];
-            const qnaPosts = [];
-
-            for (const categoryForBinding of categoryList) {
-                const postDB = await DB.execute({
-                    psmt: `select * from POST where category = ? and canceled_at is NULL order by created_at DESC limit 0, 3;`,
-                    binding: [categoryForBinding]
-                })
-
-                postDB.forEach(postDB => {
-                    const {
-                        post_id,
-                        name,
-                        title,
-                        content,
-                        views,
-                        only_member,
-                        created_at,
-                        updated_at,
-                        category
-                    } = postDB;
-
-                    const postsObj = {
-                        postId: post_id,
-                        writerName: name,
-                        title: title,
-                        content: content,
-                        views: views,
-                        onlyMember: !!only_member,
-                        createdAt: dayjs(created_at).format('YYYY-MM-DD'),
-                        updatedAt: ((updated_at) => {
-                            if (!!updated_at) {
-                                return dayjs(updated_at).format('YYYY-MM-DD')
-                            }
-                            return null;
-                        })(updated_at),
-                        category: (category)
-                    }
-                    if (category === 'notice') {
-                        noticePosts.push(postsObj);
-                    }
-                    if (category === 'knowhow') {
-                        knowhowPosts.push(postsObj);
-                    }
-                    if (category === 'reference') {
-                        referencePosts.push(postsObj);
-                    }
-                    if (category === 'memory') {
-                        memoryPosts.push(postsObj);
-                    }
-                    if (category === 'study') {
-                        studyPosts.push(postsObj);
-                    }
-                    if (category === 'qna') {
-                        qnaPosts.push(postsObj);
-                    }
-                });
-            }
-
-            res.status(200).json(Util.getReturnObject(MSG.READ_POSTDATA_SUCCESS, 200, {
-                bestPosts, noticePosts, knowhowPosts, referencePosts, memoryPosts, studyPosts, qnaPosts}));
-        } catch (error) {
-            console.log(error);
-            res.status(500).json(Util.getReturnObject(MSG.UNKNOWN_ERROR, 500, {}));
+                if (category === 'notice') {
+                    noticePosts.push(postsObj);
+                }
+                if (category === 'knowhow') {
+                    knowhowPosts.push(postsObj);
+                }
+                if (category === 'reference') {
+                    referencePosts.push(postsObj);
+                }
+                if (category === 'memory') {
+                    memoryPosts.push(postsObj);
+                }
+                if (category === 'study') {
+                    studyPosts.push(postsObj);
+                }
+                if (category === 'qna') {
+                    qnaPosts.push(postsObj);
+                }
+            });
         }
+
+        res.status(200).json(Util.getReturnObject(MSG.READ_POSTDATA_SUCCESS, 200, {
+            bestPosts, noticePosts, knowhowPosts, referencePosts, memoryPosts, studyPosts, qnaPosts
+        }));
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(Util.getReturnObject(MSG.UNKNOWN_ERROR, 500, {}));
     }
-)
-;
+});
 
 module.exports = router;
