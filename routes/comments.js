d31ec43fda4553b4e35b830859a78638dc58f404
acceptor-gyu <gseonggyu968@gmail.com>
2022-08-30 14:49:23 +0900

    childrenCount error update

--- routes/comments.js
+++ routes/comments.js
@@ -205,10 +205,10 @@ router.get('/:postId', isLoggedIn, async (req, res) => {
                     const getCommentCount = async (comment_id, parent_id) => {
                         if (comment_id === parent_id) {
                             const [childrenCount] = await DB.execute({
-                                psmt: `select count(comment_id) as childrenCount from COMMENT where parent_id = ? and canceled_at is null`,
+                                psmt: `select count(comment_id) as childrenCount from COMMENT where parent_id = ? and parent_id != comment_id and canceled_at is null`,
                                 binding: [parent_id]
                             })
-                            return childrenCount.childrenCount - 1;
+                            return childrenCount.childrenCount;
                         } else {
                             return null;
                         }
@@ -244,7 +244,7 @@ router.get('/:postId', isLoggedIn, async (req, res) => {
 
         // 삭제된 답글과 답글이 안달린 삭제된 댓글은 보여지지 않도록
         function filterCanceledReply(item) {
-            if (item == undefined || (item.comment_id === item.parent_id && item.childrenCount === -1)) {
+            if (item == undefined || (item.comment_id === item.parent_id && item.childrenCount === 0)) {
                 return false;
             }
             return true;
